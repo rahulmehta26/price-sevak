80bf295e6a5c6fa57daec9f495de1ef0e0d4b9b3
rahulmehta26 <rk6658542@gmail.com>
2026-01-29 15:35:49 +0530

    code refactor

--- src/components/icons/Dashboard.tsx
+++ src/components/icons/Dashboard.tsx
@@ -7,9 +7,9 @@ const Dashboard = ({ className }: { className?: string }) => {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             className={cn("icon", className)}
         >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
